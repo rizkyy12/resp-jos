0318d02a372f32f24e64b779beb632e4e6024167
rizkyy12 <rizky.andhika@mhs.polman-bandung.ac.id>
2021-02-18 10:33:58 +0700

    menambahkan aritmatika.cpp

---
+++ aritmatika.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int tambah(int a, int b){
+    int hasil = a + b;
+    return hasil;
+}
+
+int kurang(int a, int b){
+    int hasil = a - b;
+    return hasil;
+}
+int main(){
+    int a, b, hasil1, hasil2;
+    cout << "== aritmatika == \n";
+    cout << "1. Penjumlahan \n";
+    cin >> a;
+    cin >> b;
+    hasil1 = tambah(a, b);
+    hasil2 = kurang(a, b);
+    cout << "Penjumlahan = " << hasil1 << endl;
+    cout << "Pengurangan = " << hasil2 << endl;
+}
