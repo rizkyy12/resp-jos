1589eeaf7fc5a0c6b7fb370ef3658e676b2a4faa
rizkyy12 <rizky.andhika@mhs.polman-bandung.ac.id>
2021-02-19 09:25:59 +0700

    menambahkan function kali

--- aritmatika.cpp
+++ aritmatika.cpp
@@ -18,9 +18,13 @@ int bagi(double a, double b){
     return hasil;
 }
 
+int kali(double a, double b){
+    int hasil = a * b;
+    return hasil;
+}
 int main(){
     int a, b;
-    double hasil1, hasil2, hasil3;
+    double hasil1, hasil2, hasil3, hasil4;
     cout << "== aritmatika == \n";
     cout << "1. Penjumlahan \n";
     cin >> a;
@@ -28,7 +32,9 @@ int main(){
     hasil1 = tambah(a, b);
     hasil2 = kurang(a, b);
     hasil3 = bagi(a, b);
+    hasil4 = kali(a, b);
     cout << "Penjumlahan = " << hasil1 << endl;
     cout << "Pengurangan = " << hasil2 << endl;
     cout << "Pembagian = " << hasil3 << endl;
+    cout << "Perkalian = " << hasil4 << endl;
 }
