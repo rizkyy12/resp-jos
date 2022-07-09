76d701d308b6e7997eca182672dabd34bea79668
Rizky Andhika Akbar <65294264+rizkyy12@users.noreply.github.com>
2022-07-09 14:12:04 +0700

    Add files via upload

---
+++ Adafruit_AMG88xx_python/examples/thermal_cam.py
@@ -0,0 +1,131 @@
+from Adafruit_AMG88xx import Adafruit_AMG88xx
+import pygame
+import os
+import math
+import time
+import RPi.GPIO as GPIO
+import numpy as np
+from scipy.interpolate import griddata
+
+from colour import Color
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(27 , GPIO.OUT)
+GPIO.output(27,0)
+#low range of the sensor (this will be blue on the screen)
+MINTEMP = 19
+
+#high range of the sensor (this will be red on the screen)
+MAXTEMP = 37
+
+#how many color values we can have
+COLORDEPTH = 1024
+
+os.putenv('SDL_FBDEV', '/dev/fb1')
+pygame.init()
+
+#initialize the sensor
+sensor = Adafruit_AMG88xx()
+
+points = [(math.floor(ix / 8), (ix % 8)) for ix in range(0, 64)]
+grid_x, grid_y = np.mgrid[0:7:32j, 0:7:32j]
+
+#sensor is an 8x8 grid so lets do a square
+height = 570
+width = 570
+
+#the list of colors we can choose from
+blue = Color("indigo")
+colors = list(blue.range_to(Color("red"), COLORDEPTH))
+
+#create the array of colors
+colors = [(int(c.red * 255), int(c.green * 255), int(c.blue * 255)) for c in colors]
+
+displayPixelWidth = width / 30
+displayPixelHeight = height / 30
+
+lcd = pygame.display.set_mode((width, height),pygame.FULLSCREEN)
+
+lcd.fill((255,0,0))
+
+pygame.display.update()
+pygame.mouse.set_visible(False)
+
+lcd.fill((0,0,0))
+pygame.display.update()
+
+#some utility functions
+def constrain(val, min_val, max_val):
+    return min(max_val, max(min_val, val))
+
+def map(x, in_min, in_max, out_min, out_max):
+  return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min
+
+#let the sensor initialize
+time.sleep(.1)
+pygame.font.init()
+font = pygame.font.Font(None, 60)
+def msgtoscrn(msg,color):
+    text = font.render("Suhu: ", True,color)
+    raw = font.render(str(msg), True, color)
+    maxs = font.render(str(max(sensor.readPixels())), True, color)
+    lcd.blit(raw,[620,200])
+    lcd.blit(text,[620,150])
+    lcd.blit(maxs,[620,300])
+
+def limabelas(max_temp):
+    suhu1 = (max_temp*0.7757)+11.851
+    return suhu1
+
+def limapuluh(max_temp):
+    suhu2 = (max_temp*0.8129)+11.9
+    return suhu2
+
+def tujuhlima(max_temp):
+    suhu3 = (max_temp*0.8791)+10.588
+    return suhu3
+
+def seratus(max_temp):
+    suhu4 = (max_temp*0.9186)+13.051
+    return suhu4
+
+for x in range(0,5):
+	GPIO.output(27,1)
+	time.sleep(0.1)
+	GPIO.output(27,0)
+	time.sleep(0.1)
+while(1):
+
+	#read the pixels
+	pixels = sensor.readPixels()
+	pixels = [map(p, MINTEMP, MAXTEMP, 0, COLORDEPTH - 1) for p in pixels]
+	
+	#perdorm interpolation
+	bicubic = griddata(points, pixels, (grid_x, grid_y), method='cubic')
+	
+	#draw everything
+	for ix, row in enumerate(bicubic):
+		for jx, pixel in enumerate(row):
+			pygame.draw.rect(lcd, colors[constrain(int(pixel), 0, COLORDEPTH- 1)], (displayPixelHeight * ix, displayPixelWidth * jx, displayPixelHeight, displayPixelWidth))
+	maxx = round((max(sensor.readPixels())*0.9186)+10.551,2)
+	if maxx > 37.01:
+		GPIO.output(27,1)
+#		time.sleep(0.05)
+#		GPIO.output(27,0)
+#		time.sleep(0.05)
+	else:
+		GPIO.output(27,0)
+	msgtoscrn(str(maxx),(255,255,255))
+	pygame.display.update()
+	lcd.fill((0,0,0))
+#15 cm
+#suhu = (max_temp*0.7757)+11.851
+
+#50 cm
+#suhu = (max_temp*0.8129)+11.9
+
+#75 cm
+#suhu = (max_temp*0.8791)+10.588
+
+#100cm
+#suhu = (max_temp*0.9186)+12.051
